022c40e1e628eacf7b6e613c66c59a2431f47492
Sayantan Jana <sjana.chan@gmail.com>
2020-09-30 21:23:44 +0530

    Create index.js

---
+++ src/client/index.js
@@ -0,0 +1,12 @@
+import { validURL } from './js/urlChecker'
+import { handleSubmit } from './js/formHandler'
+import './styles/resets.scss'
+import './styles/form.scss'
+import './styles/base.scss'
+
+// export as module
+export{
+	validURL,
+	handleSubmit
+}
+ 
